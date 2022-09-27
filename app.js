5e8316ae6deb3e97ecb8d60a4779bd9a9cee0b39
amontanooo <albert.montano711@hotmail.com>
2022-09-27 17:26:55 -0400

    Add in authentication using Password
    - Add login, register and logout features
    - Detect whether a user is logged in and can access certain pages

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -39,17 +43,35 @@ const sessionConfig = {
     }
 }
 
+//Make sure sessionConfig is before passport.session
 app.use(session(sessionConfig));
 app.use(flash());
 
+// ----- Set Up Password -----
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+//How to store or retrieve User
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({ email: 'example@gmail.com', username: 'exampler' });
+    const registeredUser = await User.register(user, 'cheetos');
+    res.send(registeredUser);
+})
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home')
